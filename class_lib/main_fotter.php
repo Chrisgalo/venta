41c5861b160290223f049b2d9bd5b038826aa38c
Chrisgalo <chrisgalo09@hotmail.com>
2020-11-03 11:27:10 -0600

    first commit 03/11/2020

---
+++ class_lib/main_fotter.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+        Desarrollo de aplicaciones web.
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2017 Desarrollo de aplicaciones web | <a href=''></a></strong> | All rights reserved.
+      </footer>
